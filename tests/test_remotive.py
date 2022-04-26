9e83bce339d6f33c6f74a98a4b16a285142e7f0e
Guillaume Martin <github-commits@prof.anonaddy.com>
2022-04-26 08:53:33 +0800

    Adds more tests

--- tests/test_remotive.py
+++ tests/test_remotive.py
@@ -1,15 +1,29 @@
 import pytest
-import remotive
+from src.scrappers import remotive
 
 
-def test_get_jobs_returns_dictionary():
-
+def test_get_jobs_returns_list():
+    """ The get_jobs method should return a list
+    """
+    
     # Setup
     term = 'test'
-    category = 'Data'
 
     # Exercise
-    jobs = remotive.get_jobs(term, category)
+    jobs = remotive.get_jobs(term)
 
     # Verify
-    assert isinstance(jobs, dict) 
+    assert isinstance(jobs, list) 
+
+
+def test_get_jobs_list_contains_dictionaries():
+    """ The list returns by the get_jobs methods contains dictionaries
+    """
+    
+    # Setup
+    term = 'test'
+
+    # Exercise
+    jobs = remotive.get_jobs(term)
+    for job in jobs:
+        assert isinstance(job, dict)
