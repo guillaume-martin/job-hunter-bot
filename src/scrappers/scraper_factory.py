e79c9d2cf0b9cc216ef98631b952d56b9180cb2c
Guillaume Martin <8098412+guillaume-martin@users.noreply.github.com>
2025-11-13 12:50:48 +0800

    Adds scraper factory

---
+++ src/scrappers/scraper_factory.py
@@ -0,0 +1,9 @@
+from remotive import RemotiveScraper
+
+
+def get_scraper(site_name: str):
+    scrapers = {
+        "remotive": RemotiveScraper,
+    }
+    return scrapers.get(site_name.lower())()
+
