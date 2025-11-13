4940a2a6484d584b1867dd70e3e1813d2d4b5502
Guillaume Martin <8098412+guillaume-martin@users.noreply.github.com>
2025-11-13 12:51:59 +0800

    Refactor remotive to use BaseScraper

--- src/scrappers/remotive.py
+++ src/scrappers/remotive.py
@@ -5,7 +5,9 @@ import json
 from typing import Dict, List 
 import urllib
 
-import requests
+from base_scraper import BaseScraper
+
+from requests import request
 
 
 BASE_URL = 'https://oqubrx6zeq-3.algolianet.com/1/indexes/*/queries'
@@ -23,61 +25,60 @@ HEADERS = {
 
 LOCATIONS = '["locations:Worldwide","locations:APAC"]'
 
+class RemotiveScraper(BaseScraper):
+    """ Scraper for remotive.io jobs """
+    def __init__(self):
+        super().__init__(base_url=BASE_URL, name="Remotive")
 
-def load_jobs(payload: Dict) -> List:
-    """ Save jobs from search result in a list """
+    def _build_search_url(self, term):
+        return BASE_URL
 
-    jobs = []
-
-    data = json.dumps(payload)
-    r = requests.request('POST', BASE_URL, headers=HEADERS, data=data)
-
-    response = json.loads(r.content)
-    results = response['results']
-    jobs_list = results[0]['hits']
-
-    for job in jobs_list:
-        job_title = job.get("title", "unknown")
-        company = job.get("company_name", "unknown")
-        salary = job.get("salary", "N/A")
-        job_link = job.get("url", "unknown")
-        location = job.get("candidate_required_location", "Unknown Location")
-        utc_pub_date = datetime.utcfromtimestamp(job['publication_date'])
-        date_published = datetime.strftime(utc_pub_date, '%Y-%m-%d')
-        jobs.append({
-            "company": f"{company}({location})",
-            "title": job_title,
-            "date_published": date_published,
-            "url": job_link
-        })
-
-    return jobs 
-
-
-def get_jobs(term:str) -> List:
-
-    term_encoded = urllib.parse.quote(term)
-
-    term_encoded = urllib.parse.quote(term)
-    locations_encoded = urllib.parse.quote(LOCATIONS)
-    params = (
-        f"facetFilters=%5B{locations_encoded}%5D&"
-        "facets=&"
-        "maxValuesPerFacet=1000&"
-        "page=0&"
-        f"query=%22{term_encoded}%22&tagFilters="
-    )
+    def __build_api_payload(self, term: str) -> Dict: 
+        term_encoded = urllib.parse.quote(term)
+        locations_encoded = urllib.parse.quote(LOCATIONS)
+        params = (
+            f"facetFilters=%5B{locations_encoded}%5D&"
+            "facets=&"
+            "maxValuesPerFacet=1000&"
+            "page=0&"
+            f"query=%22{term_encoded}%22&tagFilters="
+        )
     
-    payload = {
-        "requests":[{
-            "indexName":"live_jobs",
-            "params":params
-        }]
-    }
+        payload = {
+            "requests":[{
+                "indexName":"live_jobs",
+                "params":params
+            }]
+        }
 
-    jobs = load_jobs(payload)
+        return payload
 
-    return jobs
+    def _extract_company(self, job_element):
+        return job_element.get("company_name", "unknown")
+
+    def _extract_title(self, job_element):
+        return job_element.get("title", "unknown")
+
+    def _extract_url(self, job_element):
+        return job_element.get("url", "unknown")
+
+    def _extract_date_published(self, job_element):
+        utc_pub_date = datetime.utcfromtimestamp(job_element['publication_date'])
+        date_published = datetime.strftime(utc_pub_date, '%Y-%m-%d')
+        return date_published
+   
+    def get_jobs(self, term:str) -> list:
+        search_url = self._build_search_url(term) 
+        payload = self.__build_api_payload(term)
+
+        r = request("POST", search_url, headers=HEADERS, data=json.dumps(payload))
+        response = json.loads(r.content)
+        results = response['results']
+        jobs_list = results[0]['hits']
+
+        for job in jobs_list:
+            job_details = self._extract_job_details(job)
+            self.jobs.append(job_details)
 
 
 def get_jobs_by_category(category: str) -> List:
