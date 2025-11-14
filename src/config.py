bf0ba2c962c5738f19fe63ce2cd35fa131813bff
Guillaume Martin <8098412+guillaume-martin@users.noreply.github.com>
2025-11-14 14:00:27 +0800

    Adds remoteok to config.py

--- src/config.py
+++ src/config.py
@@ -1,32 +1,33 @@
 # Searches are a list of tuples (search_term, category)
 # The categories are used in remotive.io
 searches = [
-    'database',
-    'dba',
-    'postgresql',
-    'sql',
-    'python',
-    'aws',
-    'data analyst',
-    'data engineer',
-    'backend engineer',
-    'cloud data engineer',
-    'cloud engineer',
-    'database engineer',
-    'database administrator',
-    'database manager',
-    'etl developer',
-    'devops engineer',
-    'data manager',
-    'analytics engineer',
-    'data software engineer',
-    'data architect',
-    'dataops'
+    "database",
+    "dba",
+    "postgresql",
+    "sql",
+    "python",
+    "aws",
+    "data analyst",
+    "data engineer",
+    "backend engineer",
+    "cloud data engineer",
+    "cloud engineer",
+    "database engineer",
+    "database administrator",
+    "database manager",
+    "etl developer",
+    "devops engineer",
+    "data manager",
+    "analytics engineer",
+    "data software engineer",
+    "data architect",
+    "dataops"
 ]
 
 sites = [
-    'remotive',
-    'trulyremote'
+    "remoteok"
+    "remotive",
+    "trulyremote"
 ]
 
 # Number of days since the job was published
