a9e37ba96d253b5c3323faacb8a6a79ccf33f263
Guillaume Martin <8098412+guillaume-martin@users.noreply.github.com>
2025-11-14 10:20:33 +0800

    Adds TrulyRemoteScraper to scraper_factory

--- src/scrappers/scraper_factory.py
+++ src/scrappers/scraper_factory.py
@@ -1,9 +1,11 @@
 from .remotive import RemotiveScraper
+from .trulyremote import TrulyRemoteScraper
 
 
 def get_scraper(site_name: str):
     scrapers = {
         "remotive": RemotiveScraper,
+        "trulyremote": TrulyRemoteScraper,
     }
     return scrapers.get(site_name.lower())()
 
