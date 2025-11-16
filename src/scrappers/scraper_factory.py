dba8b586e1f9937b5d6a90a8fc1454f71f162425
Guillaume Martin <8098412+guillaume-martin@users.noreply.github.com>
2025-11-16 23:32:52 +0800

    Adds 104 to scraper_factory

--- src/scrappers/scraper_factory.py
+++ src/scrappers/scraper_factory.py
@@ -1,10 +1,11 @@
 from .remoteok import RemoteOkScraper
 from .remotive import RemotiveScraper
 from .trulyremote import TrulyRemoteScraper
-
+from .tw104 import Tw104Scraper
 
 def get_scraper(site_name: str):
     scrapers = {
+        "104": Tw104Scraper,
         "remoteok": RemoteOkScraper,
         "remotive": RemotiveScraper,
         "trulyremote": TrulyRemoteScraper,
